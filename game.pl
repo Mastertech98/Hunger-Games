cb05ab169b7538c3352bede270c45fb8c3f6ac0d
Manasye Shousen Bukit <32813070+manasye@users.noreply.github.com>
2017-11-24 23:04:42 +0700

    fix lang

--- game.pl
+++ game.pl
@@ -4,10 +4,10 @@ is_game_over :- get_thirst(X), X =< 0, !.
 
 is_win :- findall(X,enemy_at(_,_,_,X),L), length(L,A), !, A == 0, !.
 
-check_game_over :- is_game_over, !, write('kamu cupu, jadinya kalah :(('), nl, halt, !.
+check_game_over :- is_game_over, !, write('You can not finish your mission ,let your soul be at peace:(('), nl, halt, !.
 check_game_over :- !.
 
-check_win :- is_win, !, write('kamu jago, jadinya menang :))'), nl, halt, !.
+check_win :- is_win, !, write('You defeat all enemy . Glorious triumph await for you :))'), nl, halt, !.
 check_win :- !.
 
 check_game_state :- check_game_over, check_win.
